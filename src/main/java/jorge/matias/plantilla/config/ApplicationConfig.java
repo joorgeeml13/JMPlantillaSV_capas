cb7b11addb4ea25345c18cec3f5ddbd410792443
Jorge Matias <jorge29364767@gmail.com>
2026-05-04 18:32:46 +0200

    Final init

--- src/main/java/jorge/matias/plantilla/config/ApplicationConfig.java
+++ src/main/java/jorge/matias/plantilla/config/ApplicationConfig.java
@@ -1,6 +1,9 @@
 package jorge.matias.plantilla.config;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,18 +11,27 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import jorge.matias.plantilla.repository.AccountRepository;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
-    private final UserDetailsService userDetailService;
+    private final AccountRepository accountRepository;
+
 
     @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> accountRepository.findById(UUID.fromString(username))
+            .orElseThrow(() -> new UsernameNotFoundException("auth.user.not_found"));
+    }
+    @Bean
     public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailService);
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
